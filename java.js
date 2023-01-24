8befad1109f6f7a148406a2c549881dd993cc207
Sindoval <sindoval321@gmail.com>
2023-01-23 23:50:29 -0300

    Tipos primitivos e variáveis

---
+++ java.js
@@ -0,0 +1,47 @@
+//let numero1 = 14;
+//let numero2 = 28;
+//let numero3 = 2;
+
+//if (numero1 > numero2 && numero1 > numero3) {
+ //   console.log(numero1);
+//}
+//else if (numero2 > numero1 && numero2 > numero3){
+ //   console.log(numero2);
+//}
+//else {
+ //   console.log(numero3)
+
+let numero1 = -9;
+let positive;
+let negative;
+
+if (numero1 > 0) {
+    positive = numero1
+    console.log("positive")
+}
+else if (numero1 < 0) {
+    negative = numero1
+    console.log("negative");
+}
+else {
+    console.log("zero");
+};
+
+
+
+
+
+const triangulo = 180;
+ const angulo1 = 55;
+ const angulo2 = 60;
+ const angulo3 = 65;
+
+if (angulo1 + angulo2 + angulo3 === triangulo) {
+    console.log ("true");
+    
+    
+} 
+else {
+    
+    console.log("Não é um triangulo")
+}
