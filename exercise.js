445d62fa3cc619e4d569a3c8c1b787e535e9ee17
Sindoval <sindoval321@gmail.com>
2023-02-05 22:41:30 -0300

    estudando get e query

---
+++ exercise.js
@@ -0,0 +1,22 @@
+const titulo = document.getElementsByTagName('h1');
+titulo[0].style.backgroundColor = 'green';
+titulo[0].style.padding = '60px';
+
+const urgente = document.getElementsByTagName('h3');
+urgente[0].style.backgroundColor = 'purple';
+urgente[1].style.backgroundColor = 'purple';
+urgente[2].style.backgroundColor = 'black';
+urgente[3].style.backgroundColor = 'black';
+
+const section1 = document.querySelectorAll('div');
+section1[0].style.backgroundColor = 'pink';
+section1[1].style.backgroundColor = 'pink';
+section1[2].style.backgroundColor = 'yellow';
+section1[3].style.backgroundColor = 'yellow';
+
+const footer = document.getElementById('footer-container');
+footer.style.backgroundColor = 'green';
+footer.style.marginBottom = '0px';
+
+const body = document.getElementsByTagName('body');
+body.style.margin = '0px';
