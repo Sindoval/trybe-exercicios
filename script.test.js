c5a80c14240a17105b54cd36975e4a44dfc0e103
Sindoval <sindoval321@gmail.com>
2023-02-26 16:41:20 -0300

    Mais testes unitarios

--- script.test.js
+++ script.test.js
@@ -1,11 +1,32 @@
-const myRemove = require('./script');
+const { myRemove, myFizzBuzz } = require('./script');
 
-test ('Teste unitario 1', () => {
-    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+describe('The Function myRemove ()', () => {
+    test ('Teste unitario 1', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+        });
+    test ('Teste unitario 2', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).toBe !== ([1, 2, 3, 4]);    
+        });
+    test ('Teste unitario 3', () => {
+        expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);    
+        });
+})
+
+////////////////////////////////////////////////////////////////////////
+describe('The Function myFizzBuzz ()', () => {
+    test ('Num divisivel por 3 e 5', () => {
+        expect(myFizzBuzz(15)).toBe('fizzbuzz');
     });
-test ('Teste unitario 2', () => {
-    expect(myRemove([1, 2, 3, 4], 3)).toBe !== ([1, 2, 3, 4]);    
+    test ('Num divisivel somente por 3', () => {
+        expect(myFizzBuzz(6)).toBe('fizz');
     });
-test ('Teste unitario 3', () => {
-    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);    
+    test ('Num divisivel apenas por 5', () => {
+        expect(myFizzBuzz(10)).toBe('buzz');
     });
+    test ('Num não é divisivel nem por 3 nem por 5', () => {
+        expect(myFizzBuzz(4)).toBe(4);
+    });
+    test ('Num não é um número', () => {
+        expect(myFizzBuzz('d')).toBe(false);
+    });
+})
